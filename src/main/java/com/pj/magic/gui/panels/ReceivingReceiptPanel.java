0bbc0539094d2f7174857b847ff6d54764cb9e3b
PJ Miranda <pendelhaven3@gmail.com>
2014-09-24 20:01:51 +0800

    Removed unit costs and quantity info table in Receiving Receipt panel

--- src/main/java/com/pj/magic/gui/panels/ReceivingReceiptPanel.java
+++ src/main/java/com/pj/magic/gui/panels/ReceivingReceiptPanel.java
@@ -7,19 +7,12 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.ComponentAdapter;
 import java.awt.event.ComponentEvent;
-import java.math.BigDecimal;
 
-import javax.swing.BorderFactory;
 import javax.swing.JLabel;
 import javax.swing.JScrollPane;
-import javax.swing.JTable;
 import javax.swing.JToolBar;
-import javax.swing.event.ListSelectionEvent;
-import javax.swing.event.ListSelectionListener;
 import javax.swing.event.TableModelEvent;
 import javax.swing.event.TableModelListener;
-import javax.swing.table.AbstractTableModel;
-import javax.swing.table.DefaultTableCellRenderer;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -29,9 +22,7 @@ import org.springframework.stereotype.Component;
 import com.pj.magic.gui.component.MagicToolBar;
 import com.pj.magic.gui.component.MagicToolBarButton;
 import com.pj.magic.gui.tables.ReceivingReceiptItemsTable;
-import com.pj.magic.model.Product;
 import com.pj.magic.model.ReceivingReceipt;
-import com.pj.magic.model.Unit;
 import com.pj.magic.service.PaymentTermService;
 import com.pj.magic.service.PricingSchemeService;
 import com.pj.magic.service.ProductService;
@@ -61,7 +52,6 @@ public class ReceivingReceiptPanel extends AbstractMagicPanel {
 	private JLabel totalAmountField;
 	private JLabel totalDiscountedAmountField;
 	private JLabel totalNetAmountField;
-	private UnitCostsAndQuantitiesTableModel unitCostsAndQuantitiesTableModel = new UnitCostsAndQuantitiesTableModel();
 	private MagicToolBarButton postButton;
 	
 	@Override
@@ -72,7 +62,6 @@ public class ReceivingReceiptPanel extends AbstractMagicPanel {
 		
 		focusOnItemsTableWhenThisPanelIsDisplayed();
 		updateTotalAmountFieldWhenItemsTableChanges();
-		initializeUnitCostsAndQuantitiesTable();
 	}
 
 	private void focusOnItemsTableWhenThisPanelIsDisplayed() {
@@ -294,18 +283,6 @@ public class ReceivingReceiptPanel extends AbstractMagicPanel {
 
 		currentRow++;
 		
-		c.fill = GridBagConstraints.BOTH;
-		c.weightx = c.weighty = 0.0;
-		c.gridx = 0;
-		c.gridy = currentRow;
-		c.gridwidth = 7;
-		c.anchor = GridBagConstraints.CENTER;
-		JScrollPane infoTableScrollPane = new JScrollPane(new UnitPricesAndQuantitiesTable());
-		infoTableScrollPane.setPreferredSize(new Dimension(500, 65));
-		add(infoTableScrollPane, c);
-		
-		currentRow++;
-		
 		c.weightx = c.weighty = 0.0;
 		c.fill = GridBagConstraints.NONE;
 		c.gridx = 4;
@@ -360,30 +337,6 @@ public class ReceivingReceiptPanel extends AbstractMagicPanel {
 		
 	}
 	
-	private void initializeUnitCostsAndQuantitiesTable() {
-		itemsTable.getSelectionModel().addListSelectionListener(new ListSelectionListener() {
-			
-			@Override
-			public void valueChanged(ListSelectionEvent e) {
-				updateUnitCostsAndQuantitiesTable();
-			}
-		});
-	}
-	
-	private void updateUnitCostsAndQuantitiesTable() {
-		if (itemsTable.getSelectedRow() == -1) {
-			unitCostsAndQuantitiesTableModel.setProduct(null);
-			return;
-		}
-		
-		Product product = itemsTable.getCurrentlySelectedRowItem().getItem().getProduct();
-		if (product != null && product.isValid()) {
-			unitCostsAndQuantitiesTableModel.setProduct(productService.getProduct(product.getId()));
-		} else {
-			unitCostsAndQuantitiesTableModel.setProduct(null);
-		}
-	}
-	
 	private JToolBar createToolBar() {
 		MagicToolBar toolBar = new MagicToolBar();
 		addBackButton(toolBar);
@@ -402,146 +355,6 @@ public class ReceivingReceiptPanel extends AbstractMagicPanel {
 		return toolBar;
 	}
 	
-	private class UnitPricesAndQuantitiesTable extends JTable {
-		
-		public UnitPricesAndQuantitiesTable() {
-			super(unitCostsAndQuantitiesTableModel);
-			setTableHeader(null);
-			setRowHeight(20);
-			setShowGrid(false);
-			setDefaultRenderer(Object.class, new DefaultTableCellRenderer() {
-				
-				@Override
-				public java.awt.Component getTableCellRendererComponent(
-						JTable table, Object value, boolean isSelected,
-						boolean hasFocus, int row, int column) {
-					super.getTableCellRendererComponent(table, value, isSelected, hasFocus,
-							row, column);
-					setBorder(BorderFactory.createEmptyBorder(0, 5, 0, 5));
-					return this;
-				}
-				
-			});
-		}
-		
-	}
-	
-	private class UnitCostsAndQuantitiesTableModel extends AbstractTableModel {
-
-		private Product product;
-		
-		public void setProduct(Product product) {
-			this.product = product;
-			fireTableDataChanged();
-		}
-		
-		@Override
-		public int getRowCount() {
-			return 3;
-		}
-
-		@Override
-		public int getColumnCount() {
-			return 6;
-		}
-
-		@Override
-		public Object getValueAt(int rowIndex, int columnIndex) {
-			switch (rowIndex) {
-			case 0:
-				if (columnIndex == 0) {
-					return Unit.CASE;
-				} else if (columnIndex == 3) {
-					return Unit.TIE;
-				}
-				break;
-			case 1:
-				if (columnIndex == 0) {
-					return Unit.CARTON;
-				} else if (columnIndex == 3) {
-					return Unit.DOZEN;
-				}
-				break;
-			case 2:
-				switch (columnIndex) {
-				case 0:
-					return Unit.PIECES;
-				case 3:
-				case 4:
-				case 5:
-					return "";
-				}
-				break;
-			}
-			
-			if (product == null) {
-				switch (columnIndex) {
-				case 1:
-				case 4:
-					return "0";
-				case 2:
-				case 5:
-					return FormatterUtil.formatAmount(BigDecimal.ZERO);
-				}
-			}
-			
-			product = productService.findProductByCode(product.getCode());
-			
-			if (rowIndex == 0) {
-				switch (columnIndex) {
-				case 1:
-					return String.valueOf(product.getUnitQuantity(Unit.CASE));
-				case 2:
-					BigDecimal unitCost = product.getGrossCost(Unit.CASE);
-					if (unitCost == null) {
-						unitCost = BigDecimal.ZERO;
-					}
-					return FormatterUtil.formatAmount(unitCost);
-				case 4:
-					return String.valueOf(product.getUnitQuantity(Unit.TIE));
-				case 5:
-					unitCost = product.getUnitPrice(Unit.TIE);
-					if (unitCost == null) {
-						unitCost = BigDecimal.ZERO;
-					}
-					return FormatterUtil.formatAmount(unitCost);
-				}
-			} else if (rowIndex == 1) {
-				switch (columnIndex) {
-				case 1:
-					return String.valueOf(product.getUnitQuantity(Unit.CARTON));
-				case 2:
-					BigDecimal unitCost = product.getGrossCost(Unit.CARTON);
-					if (unitCost == null) {
-						unitCost = BigDecimal.ZERO;
-					}
-					return FormatterUtil.formatAmount(unitCost);
-				case 4:
-					return String.valueOf(product.getUnitQuantity(Unit.DOZEN));
-				case 5:
-					unitCost = product.getUnitPrice(Unit.DOZEN);
-					if (unitCost == null) {
-						unitCost = BigDecimal.ZERO;
-					}
-					return FormatterUtil.formatAmount(unitCost);
-				}
-			} else if (rowIndex == 2) {
-				switch (columnIndex) {
-				case 1:
-					return String.valueOf(product.getUnitQuantity(Unit.PIECES));
-				case 2:
-					BigDecimal unitCost = product.getGrossCost(Unit.PIECES);
-					if (unitCost == null) {
-						unitCost = BigDecimal.ZERO;
-					}
-					return FormatterUtil.formatAmount(unitCost);
-				}
-			}
-			return "";
-		}
-		
-	}
-
 	private void postReceivingReceipt() {
 		if (itemsTable.isEditing()) {
 			itemsTable.getCellEditor().cancelCellEditing();
