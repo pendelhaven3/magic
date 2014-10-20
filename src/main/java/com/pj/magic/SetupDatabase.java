9791c49d2daacb6780645a4f268745ad0d0f7945
PJ Miranda <pendelhaven3@gmail.com>
2014-10-20 22:35:40 +0800

    Created database setup class

--- src/main/java/com/pj/magic/SetupDatabase.java
+++ src/main/java/com/pj/magic/SetupDatabase.java
@@ -7,22 +7,22 @@ import javax.sql.DataSource;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
-public class TablesCreator {
+public class SetupDatabase {
 
 	private DataSource dataSource;
 	
 	public static void main(String[] args) throws Exception {
 		@SuppressWarnings("resource")
-		ApplicationContext context = new ClassPathXmlApplicationContext("datasource-create.xml");
-		TablesCreator tablesCreator = context.getBean(TablesCreator.class);
-		tablesCreator.run();
+		ApplicationContext context = new ClassPathXmlApplicationContext("datasource-setup.xml");
+		SetupDatabase setupDatabase = context.getBean(SetupDatabase.class);
+		setupDatabase.run();
 	}
 
 	private void run() throws Exception {
 		try (
 			Connection conn = dataSource.getConnection();
 		) {
-			ScriptFileRunner.runScriptFiles(conn, "tables-mysql.sql");
+			ScriptFileRunner.runScriptFiles(conn, "tables.sql", "initial_data.sql");
 		}
 	}
 	
