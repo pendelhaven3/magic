d1c0aceab6ad1ae0d6dbbc41b442f51e424d51f1
PJ Miranda <pendelhaven3@gmail.com>
2014-11-10 15:53:16 +0800

    Added version system parameter to initial data SQL

--- src/main/resources/sql/initial_data.sql
+++ src/main/resources/sql/initial_data.sql
@@ -6,6 +6,8 @@ insert into SEQUENCE (NAME) values ('RECEIVING_RECEIPT_NO_SEQ');
 insert into SEQUENCE (NAME) values ('ADJUSTMENT_OUT_NO_SEQ');
 insert into SEQUENCE (NAME) values ('ADJUSTMENT_IN_NO_SEQ');
 
+insert into SYSTEM_PARAMETER (NAME, VALUE) values ('VERSION', '0.17.0');
+
 insert into PRICING_SCHEME (ID, NAME) values (1, 'CANVASSER');
 
 insert into USER (ID, USERNAME, PASSWORD, SUPERVISOR_IND) values (1, 'ADMIN', 'jGl25bVBBBW96Qi9Te4V37Fnqchz/Eu4qB9vKrRIqRg=', 'Y');
