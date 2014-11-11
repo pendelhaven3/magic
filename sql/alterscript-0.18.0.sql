3ae0b6af1db944254b5fbc1388b9a8afaab3302b
PJ Miranda <pendelhaven3@gmail.com>
2014-11-11 09:58:22 +0800

    Updated version to 0.18.0

--- sql/alterscript-0.18.0.sql
+++ sql/alterscript-0.18.0.sql
@@ -1,3 +1,4 @@
 alter table SALES_INVOICE add VAT_AMOUNT numeric(8,2) null;
 update SALES_INVOICE set VAT_AMOUNT = 0;
 alter table SALES_INVOICE modify column VAT_AMOUNT numeric(8,2) not null;
+update SYSTEM_PARAMETER set VALUE = '0.18.0' where NAME = 'VERSION';
