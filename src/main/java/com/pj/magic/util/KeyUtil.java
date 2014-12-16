98c9e95fd4084e5269386be99c04a6551a8a9516
PJ Miranda <pendelhaven3@gmail.com>
2014-12-16 19:40:17 +0800

    Removed unused KeyUtil methods

--- src/main/java/com/pj/magic/util/KeyUtil.java
+++ src/main/java/com/pj/magic/util/KeyUtil.java
@@ -1,29 +1,11 @@
 package com.pj.magic.util;
 
-import java.awt.AWTException;
-import java.awt.Robot;
 import java.awt.event.KeyEvent;
 
 import javax.swing.KeyStroke;
 
 public class KeyUtil {
 
-	public static void simulateTabKey() {
-		try {
-			new Robot().keyPress(KeyEvent.VK_TAB);
-		} catch (AWTException e) {
-			e.printStackTrace();
-		}
-	}
-	
-	public static void simulateDownKey() {
-		try {
-			new Robot().keyPress(KeyEvent.VK_DOWN);
-		} catch (AWTException e) {
-			e.printStackTrace();
-		}
-	}
-	
 	public static boolean isAlphaNumericKeyCode(int keyCode) {
 		return (keyCode >= KeyEvent.VK_0 && keyCode <= KeyEvent.VK_9)
 				|| (keyCode >= KeyEvent.VK_A && keyCode <= KeyEvent.VK_Z);
@@ -37,8 +19,4 @@ public class KeyUtil {
 		return KeyStroke.getKeyStroke(KeyEvent.VK_ENTER, 0);
 	}
 
-	public static KeyStroke getF10Key() {
-		return KeyStroke.getKeyStroke(KeyEvent.VK_F10, 0);
-	}
-	
 }
