3dad0f9a9462e0226ff7fcdec19649772f995d0d
PJ Miranda <pendelhaven3@gmail.com>
2015-01-03 11:15:53 +0800

    Display Posted By in Payments list

--- src/main/java/com/pj/magic/gui/tables/models/PaymentsTableModel.java
+++ src/main/java/com/pj/magic/gui/tables/models/PaymentsTableModel.java
@@ -13,12 +13,14 @@ import com.pj.magic.util.FormatterUtil;
 @Component
 public class PaymentsTableModel extends AbstractTableModel {
 
-	private static final String[] columnNames = {"Payment No.", "Customer", "Create Date", "Encoder", "Status"};
+	private static final String[] columnNames = 
+		{"Payment No.", "Customer", "Create Date", "Encoder", "Status", "Posted By"};
 	private static final int PAYMENT_NUMBER_COLUMN_INDEX = 0;
 	private static final int CUSTOMER_COLUMN_INDEX = 1;
 	private static final int CREATE_DATE_COLUMN_INDEX = 2;
 	private static final int ENCODER_COLUMN_INDEX = 3;
 	private static final int STATUS_COLUMN_INDEX = 4;
+	private static final int POSTED_BY_COLUMN_INDEX = 5;
 	
 	private List<Payment> payments = new ArrayList<>();
 	
@@ -46,6 +48,8 @@ public class PaymentsTableModel extends AbstractTableModel {
 			return payment.getEncoder().getUsername();
 		case STATUS_COLUMN_INDEX:
 			return payment.getStatus();
+		case POSTED_BY_COLUMN_INDEX:
+			return payment.isPosted() ? payment.getPostedBy().getUsername() : null;
 		default:
 			throw new RuntimeException("Fetching invalid column index: " + columnIndex);
 		}
