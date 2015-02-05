e59929a9df855f0b2ad44a450edf3e74278cdf73
PJ Miranda <pendelhaven3@gmail.com>
2015-02-05 10:43:47 +0800

    Fixed alterscript for new Purchase Return Bad Stock tables

--- sql/alterscript-1.23.0.sql
+++ sql/alterscript-1.23.0.sql
@@ -1,27 +1,27 @@
 insert into SEQUENCE (NAME) values ('PURCHASE_RETURN_BAD_STOCK_NO_SEQ');
 
-create table BAD_PURCHASE_RETURN (
+create table PURCHASE_RETURN_BAD_STOCK (
   ID integer auto_increment,
-  BAD_PURCHASE_RETURN_NO integer not null,
+  PURCHASE_RETURN_BAD_STOCK_NO integer not null,
   SUPPLIER_ID integer not null,
   POST_IND char(1) default 'N' not null,
   POST_DT date null,
   POST_BY integer null,
   REMARKS varchar(100) null,
-  constraint BAD_PURCHASE_RETURN$PK primary key (ID),
-  constraint BAD_PURCHASE_RETURN$UK unique (BAD_PURCHASE_RETURN_NO),
-  constraint BAD_PURCHASE_RETURN$FK foreign key (SUPPLIER_ID) references SUPPLIER (ID),
-  constraint BAD_PURCHASE_RETURN$FK2 foreign key (POST_BY) references USER (ID)
+  constraint PURCHASE_RETURN_BAD_STOCK$PK primary key (ID),
+  constraint PURCHASE_RETURN_BAD_STOCK$UK unique (PURCHASE_RETURN_BAD_STOCK_NO),
+  constraint PURCHASE_RETURN_BAD_STOCK$FK foreign key (SUPPLIER_ID) references SUPPLIER (ID),
+  constraint PURCHASE_RETURN_BAD_STOCK$FK2 foreign key (POST_BY) references USER (ID)
 );
 
-create table BAD_PURCHASE_RETURN_ITEM (
+create table PURCHASE_RETURN_BAD_STOCK_ITEM (
   ID integer auto_increment,
-  BAD_PURCHASE_RETURN_ID integer not null,
+  PURCHASE_RETURN_BAD_STOCK_ID integer not null,
   PRODUCT_ID integer not null,
   UNIT char(3) not null,
   QUANTITY integer not null,
   UNIT_COST numeric(10, 2) not null,
-  constraint BAD_PURCHASE_RETURN_ITEM$PK primary key (ID),
-  constraint BAD_PURCHASE_RETURN_ITEM$FK foreign key (BAD_PURCHASE_RETURN_ID) references BAD_PURCHASE_RETURN (ID),
-  constraint BAD_PURCHASE_RETURN_ITEM$FK2 foreign key (PRODUCT_ID) references PRODUCT (ID)
+  constraint PURCHASE_RETURN_BAD_STOCK_ITEM$PK primary key (ID),
+  constraint PURCHASE_RETURN_BAD_STOCK_ITEM$FK foreign key (PURCHASE_RETURN_BAD_STOCK_ID) references PURCHASE_RETURN_BAD_STOCK (ID),
+  constraint PURCHASE_RETURN_BAD_STOCK_ITEM$FK2 foreign key (PRODUCT_ID) references PRODUCT (ID)
 );
