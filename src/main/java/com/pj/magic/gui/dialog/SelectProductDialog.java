a99b3cadc87115ed2a9cfbc9ea32322eb831cb62
PJ Miranda <pendelhaven3@gmail.com>
2015-03-16 10:09:48 +0800

    Moved table models used in Select Product dialog inside SelectProductDialog.java

--- src/main/java/com/pj/magic/gui/dialog/SelectProductDialog.java
+++ src/main/java/com/pj/magic/gui/dialog/SelectProductDialog.java
@@ -8,7 +8,6 @@ import java.awt.event.ActionEvent;
 import java.awt.event.KeyEvent;
 import java.util.List;
 
-import javax.annotation.PostConstruct;
 import javax.swing.AbstractAction;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
@@ -16,6 +15,7 @@ import javax.swing.JTable;
 import javax.swing.KeyStroke;
 import javax.swing.event.ListSelectionEvent;
 import javax.swing.event.ListSelectionListener;
+import javax.swing.table.AbstractTableModel;
 
 import org.apache.commons.lang.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -25,14 +25,14 @@ import com.pj.magic.Constants;
 import com.pj.magic.gui.component.DoubleClickMouseAdapter;
 import com.pj.magic.gui.tables.MagicListTable;
 import com.pj.magic.gui.tables.models.ProductsTableModel;
-import com.pj.magic.gui.tables.models.UnitCostsAndQuantitiesTableModel;
-import com.pj.magic.gui.tables.models.UnitPricesAndQuantitiesTableModel;
 import com.pj.magic.model.PricingScheme;
 import com.pj.magic.model.Product;
 import com.pj.magic.model.Supplier;
+import com.pj.magic.model.Unit;
 import com.pj.magic.model.search.ProductSearchCriteria;
 import com.pj.magic.service.ProductService;
 import com.pj.magic.util.ComponentUtil;
+import com.pj.magic.util.FormatterUtil;
 
 @Component
 public class SelectProductDialog extends MagicDialog {
@@ -44,10 +44,8 @@ public class SelectProductDialog extends MagicDialog {
 	@Autowired private ProductService productService;
 
 	private ProductsTableModel productsTableModel = new ProductsTableModel();
-	private UnitPricesAndQuantitiesTableModel unitPricesAndQuantitiesTableModel = 
-			new UnitPricesAndQuantitiesTableModel();
-	private UnitCostsAndQuantitiesTableModel unitCostsAndQuantitiesTableModel =
-			new UnitCostsAndQuantitiesTableModel();
+	private UnitPricesAndQuantitiesTableModel unitPricesAndQuantitiesTableModel;
+	private UnitCostsAndQuantitiesTableModel unitCostsAndQuantitiesTableModel;
 	private MagicListTable productsTable;
 	private JTable unitPricesAndQuantitiesTable;
 	private JTable unitCostsAndQuantitiesTable;
@@ -58,9 +56,10 @@ public class SelectProductDialog extends MagicDialog {
 		setSize(500, 450);
 		setLocationRelativeTo(null);
 		setTitle("Select Product");
+		
+		initialize();
 	}
 
-	@PostConstruct
 	public void initialize() {
 		productsTable = new MagicListTable(productsTableModel);
 		productsTable.getColumnModel().getColumn(ProductsTableModel.CODE_COLUMN_INDEX)
@@ -68,7 +67,10 @@ public class SelectProductDialog extends MagicDialog {
 		productsTable.getColumnModel().getColumn(ProductsTableModel.DESCRIPTION_COLUMN_INDEX)
 			.setPreferredWidth(300);
 		
+		unitPricesAndQuantitiesTableModel = new UnitPricesAndQuantitiesTableModel();
 		unitPricesAndQuantitiesTable = new MagicListTable(unitPricesAndQuantitiesTableModel);
+		
+		unitCostsAndQuantitiesTableModel = new UnitCostsAndQuantitiesTableModel();
 		unitCostsAndQuantitiesTable = new MagicListTable(unitCostsAndQuantitiesTableModel);
 		
 		layoutComponents();
@@ -245,6 +247,129 @@ public class SelectProductDialog extends MagicDialog {
 			productsTable.selectFirstRow();
 		}
 		((CardLayout)infoTablePanel.getLayout()).show(infoTablePanel, UNIT_PRICE_INFO_TABLE);
+	}
+	
+	private class UnitPricesAndQuantitiesTableModel extends AbstractTableModel {
+
+		private static final int UNIT_COLUMN_INDEX = 0;
+		private static final int QUANTITY_COLUMN_INDEX = 1;
+		private static final int UNIT_PRICE_COLUMN_INDEX = 2;
+		
+		private final String[] columnNames = {"Unit", "Quantity", "Price"};
+		private final String[] units = {Unit.CASE, Unit.TIE, Unit.CARTON, Unit.DOZEN, Unit.PIECES};
+		
+		private Product product;
+		
+		public void setProduct(Product product) {
+			this.product = product;
+			fireTableDataChanged();
+		}
+		
+		@Override
+		public int getRowCount() {
+			return 5;
+		}
+
+		@Override
+		public int getColumnCount() {
+			return columnNames.length;
+		}
+
+		@Override
+		public Object getValueAt(int rowIndex, int columnIndex) {
+			String unit = units[rowIndex];
+
+			switch (columnIndex) {
+			case UNIT_COLUMN_INDEX:
+				return unit;
+			case QUANTITY_COLUMN_INDEX:
+				return (product != null) ? product.getUnitQuantity(unit) : 0;
+			case UNIT_PRICE_COLUMN_INDEX:
+				if (product != null && product.hasUnit(unit)) {
+					return FormatterUtil.formatAmount(product.getUnitPrice(unit));
+				} else {
+					return "0.00";
+				}
+			default:
+				throw new RuntimeException("Fetching invalid column index: " + columnIndex);
+			}
+		}
+
+		@Override
+		public String getColumnName(int column) {
+			return columnNames[column];
+		}
+		
+		@Override
+		public Class<?> getColumnClass(int columnIndex) {
+			if (columnIndex == UNIT_PRICE_COLUMN_INDEX) {
+				return Number.class;
+			} else {
+				return Object.class;
+			}
+		}
+	
+	}
+	
+	private class UnitCostsAndQuantitiesTableModel extends AbstractTableModel {
+
+		private static final int UNIT_COLUMN_INDEX = 0;
+		private static final int QUANTITY_COLUMN_INDEX = 1;
+		private static final int UNIT_COST_COLUMN_INDEX = 2;
+		private final String[] columnNames = {"Unit", "Quantity", "Cost"};
+		private final String[] units = {Unit.CASE, Unit.TIE, Unit.CARTON, Unit.DOZEN, Unit.PIECES};
+		
+		private Product product;
+		
+		public void setProduct(Product product) {
+			this.product = product;
+			fireTableDataChanged();
+		}
+		
+		@Override
+		public int getRowCount() {
+			return 5;
+		}
+
+		@Override
+		public int getColumnCount() {
+			return columnNames.length;
+		}
+
+		@Override
+		public Object getValueAt(int rowIndex, int columnIndex) {
+			String unit = units[rowIndex];
+			
+			switch (columnIndex) {
+			case UNIT_COLUMN_INDEX:
+				return unit;
+			case QUANTITY_COLUMN_INDEX:
+				return (product != null) ? product.getUnitQuantity(unit) : "0";
+			case UNIT_COST_COLUMN_INDEX:
+				if (product != null && product.hasUnit(unit)) {
+					return FormatterUtil.formatAmount(product.getGrossCost(unit));
+				} else {
+					return "0.00";
+				}
+			default:
+				throw new RuntimeException("Fetching invalid column index: " + columnIndex);
+			}
+		}
+
+		@Override
+		public String getColumnName(int column) {
+			return columnNames[column];
+		}
+		
+		@Override
+		public Class<?> getColumnClass(int columnIndex) {
+			if (columnIndex == UNIT_COST_COLUMN_INDEX) {
+				return Number.class;
+			} else {
+				return Object.class;
+			}
+		}
+		
 	}
 	
 }
