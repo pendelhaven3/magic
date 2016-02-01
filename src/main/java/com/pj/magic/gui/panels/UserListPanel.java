efa7ef1b962a339018f8eec5cf86b7a24ae291f0
PJ Miranda <pendelhaven3@gmail.com>
2016-02-01 11:24:17 +0800

    Improve UserListPanel code

--- src/main/java/com/pj/magic/gui/panels/UserListPanel.java
+++ src/main/java/com/pj/magic/gui/panels/UserListPanel.java
@@ -4,50 +4,54 @@ import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.awt.event.KeyEvent;
+import java.net.URL;
 import java.util.List;
 
-import javax.swing.AbstractAction;
+import javax.swing.Box;
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
-import javax.swing.JTable;
-import javax.swing.KeyStroke;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-import com.pj.magic.gui.component.DoubleClickMouseAdapter;
+import com.pj.magic.gui.component.CustomAction;
 import com.pj.magic.gui.component.MagicToolBar;
 import com.pj.magic.gui.component.MagicToolBarButton;
 import com.pj.magic.gui.tables.MagicListTable;
-import com.pj.magic.gui.tables.models.UsersTableModel;
+import com.pj.magic.gui.tables.models.ListBackedTableModel;
 import com.pj.magic.model.User;
 import com.pj.magic.service.UserService;
-import com.pj.magic.util.ComponentUtil;
 
 @Component
 public class UserListPanel extends StandardMagicPanel {
 
-	private static final String EDIT_USER_ACTION_NAME = "editUser";
+	private static final int USERNAME_COLUMN_INDEX = 0;
+	private static final int SUPERVISOR_COLUMN_INDEX = 1;
+	private static final int MODIFY_PRICING_COLUMN_INDEX = 2;
+	
+	private static final ImageIcon checkIcon = createImageIcon("/images/small_check.png");
 	
 	@Autowired private UserService userService;
-	@Autowired private UsersTableModel tableModel;
 	
-	private JTable table;
+	private MagicListTable table;
+	private UsersTableModel tableModel;
 	
+	@Override
+	protected void initializeComponents() {
+		tableModel = new UsersTableModel();
+		table = new MagicListTable(tableModel);
+		
+		focusOnComponentWhenThisPanelIsDisplayed(table);
+	}
+
 	public void updateDisplay() {
 		List<User> users = userService.getAllUsers();
-		tableModel.setUsers(users);
+		tableModel.setItems(users);
 		if (!users.isEmpty()) {
 			table.changeSelection(0, 0, false, false);
 		}
-	}
-
-	@Override
-	protected void initializeComponents() {
-		table = new MagicListTable(tableModel);
-		focusOnComponentWhenThisPanelIsDisplayed(table);
 	}
 
 	@Override
@@ -59,10 +63,11 @@ public class UserListPanel extends StandardMagicPanel {
 		GridBagConstraints c = new GridBagConstraints();
 		c.gridx = 0;
 		c.gridy = currentRow;
-		mainPanel.add(ComponentUtil.createFiller(1, 5), c);
+		mainPanel.add(Box.createVerticalStrut(5), c);
 		
 		currentRow++;
 		
+		c = new GridBagConstraints();
 		c.fill = GridBagConstraints.BOTH;
 		c.weightx = c.weighty = 1.0;
 		c.gridx = 0;
@@ -72,26 +77,17 @@ public class UserListPanel extends StandardMagicPanel {
 
 	@Override
 	protected void registerKeyBindings() {
-		table.getInputMap().put(KeyStroke.getKeyStroke(KeyEvent.VK_ENTER, 0), EDIT_USER_ACTION_NAME);
-		table.getActionMap().put(EDIT_USER_ACTION_NAME, new AbstractAction() {
+		table.onEnterKeyAndDoubleClick(new CustomAction() {
 			
 			@Override
-			public void actionPerformed(ActionEvent e) {
-				selectUser();
-			}
-		});
-		
-		table.addMouseListener(new DoubleClickMouseAdapter() {
-			
-			@Override
-			protected void onDoubleClick() {
+			public void doAction() {
 				selectUser();
 			}
 		});
 	}
 
 	protected void selectUser() {
-		User user = tableModel.getUser(table.getSelectedRow());
+		User user = tableModel.getItem(table.getSelectedRow());
 		getMagicFrame().switchToEditUserPanel(user);
 	}
 
@@ -116,6 +112,51 @@ public class UserListPanel extends StandardMagicPanel {
 		});
 		
 		toolBar.add(postButton);
+	}
+	
+	private class UsersTableModel extends ListBackedTableModel<User> {
+
+		private final String[] columnNames = {"Username", "Supervisor", "Modify Pricing?"};
+		
+		@Override
+		public Object getValueAt(int rowIndex, int columnIndex) {
+			User user = getItem(rowIndex);
+			switch (columnIndex) {
+			case USERNAME_COLUMN_INDEX:
+				return user.getUsername();
+			case SUPERVISOR_COLUMN_INDEX:
+				return user.isSupervisor() ? checkIcon : null;
+			case MODIFY_PRICING_COLUMN_INDEX:
+				return user.isSupervisor() || user.isModifyPricing() ? checkIcon : null;
+			default:
+				return null;
+			}
+		}
+
+		@Override
+		protected String[] getColumnNames() {
+			return columnNames;
+		}
+		
+		@Override
+		public Class<?> getColumnClass(int columnIndex) {
+			if (columnIndex == SUPERVISOR_COLUMN_INDEX || columnIndex == MODIFY_PRICING_COLUMN_INDEX) {
+				return ImageIcon.class;
+			} else {
+				return Object.class;
+			}
+		}
+		
+	}
+	
+	private static ImageIcon createImageIcon(String path) {
+		URL imgURL = UserListPanel.class.getResource(path);
+		if (imgURL != null) {
+			return new ImageIcon(imgURL);
+		} else {
+			System.err.println("Couldn't find file: " + path);
+			return null;
+		}
 	}
 	
 }
