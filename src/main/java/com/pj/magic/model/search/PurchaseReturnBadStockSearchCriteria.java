7f68168689e7cf0e50c6a0b50a1dfdac5c120470
PJ Miranda <pendelhaven3@gmail.com>
2020-01-24 22:24:01 +0800

    Use lombok getter/setter for PurchaseReturnBadStockSearchCriteria

--- src/main/java/com/pj/magic/model/search/PurchaseReturnBadStockSearchCriteria.java
+++ src/main/java/com/pj/magic/model/search/PurchaseReturnBadStockSearchCriteria.java
@@ -4,43 +4,16 @@ import java.util.Date;
 
 import com.pj.magic.model.Supplier;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class PurchaseReturnBadStockSearchCriteria {
 
 	private Long purchaseReturnBadStockNumber;
 	private Boolean posted;
 	private Supplier supplier;
 	private Date postDate;
-
-	public Boolean getPosted() {
-		return posted;
-	}
-
-	public void setPosted(Boolean posted) {
-		this.posted = posted;
-	}
-
-	public Date getPostDate() {
-		return postDate;
-	}
-
-	public void setPostDate(Date postDate) {
-		this.postDate = postDate;
-	}
-
-	public Long getPurchaseReturnBadStockNumber() {
-		return purchaseReturnBadStockNumber;
-	}
-
-	public void setPurchaseReturnBadStockNumber(Long purchaseReturnBadStockNumber) {
-		this.purchaseReturnBadStockNumber = purchaseReturnBadStockNumber;
-	}
-
-	public Supplier getSupplier() {
-		return supplier;
-	}
-
-	public void setSupplier(Supplier supplier) {
-		this.supplier = supplier;
-	}
 
 }
